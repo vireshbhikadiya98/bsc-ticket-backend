fdc29d67ed8c7dc01b51ae724c3b7778a42e2d83
vireshbhikadiya98 <vireshbhikadiya98@gmail.com>
2026-07-28 12:00:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const profileRoutes = require('./profile');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'https://bsc-ticket-frontend.vercel.app/',
+    'http://localhost:5173',
+    'http://localhost:3000'
+    ],
   credentials: true
 }));
 
